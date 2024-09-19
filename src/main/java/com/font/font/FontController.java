4da8414b919e5ffe88f51d417306494f63329719
Luixij <149379689+Luixij@users.noreply.github.com>
2024-09-19 17:46:47 +0200

    Update FontController.java

--- src/main/java/com/font/font/FontController.java
+++ src/main/java/com/font/font/FontController.java
@@ -30,58 +30,29 @@ public class FontController {
         this.apiKey = dotenv.get("API_KEY");
     }
 
-    @GetMapping("/fonts")
-    @ResponseBody
-    public Map<String, List<Map<String, String>>> getFonts() {
-        List<Map<String, String>> allFonts = new ArrayList<>();
-        Map<String, String> combinedFonts = new HashMap<>();
+   @GetMapping("/fonts")
+@ResponseBody
+public Map<String, List<Map<String, String>>> getFonts() {
+    List<Map<String, String>> allFonts = new ArrayList<>();
+    Map<String, String> combinedFonts = new HashMap<>();
 
-        // Añadir combinaciones de fuentes
-        combinedFonts.put("Merriweather", "Open Sans");
-        combinedFonts.put("Playfair Display", "Montserrat");
-        combinedFonts.put("Lato", "Roboto");
-        combinedFonts.put("Raleway", "Roboto Slab");
-        combinedFonts.put("Amatic SC", "Josefin Sans");
+    // Aquí defines las combinaciones de fuentes que deseas
+    combinedFonts.put("Merriweather", "Open Sans");
+    combinedFonts.put("Playfair Display", "Montserrat");
+    combinedFonts.put("Lato", "Roboto");
+    combinedFonts.put("Raleway", "Roboto Slab");
+    combinedFonts.put("Amatic SC", "Josefin Sans");
 
-        // Añadir las combinaciones al principio
-        for (Map.Entry<String, String> entry : combinedFonts.entrySet()) {
-            Map<String, String> fontEntry = new HashMap<>();
-            fontEntry.put("font", entry.getKey());
-            fontEntry.put("pair", entry.getValue());
-            allFonts.add(fontEntry);
-        }
-
-        // Añadir un separador para "Resto de fuentes"
-        Map<String, String> separator = new HashMap<>();
-        separator.put("font", "Resto de fuentes");
-        separator.put("pair", "");
-        allFonts.add(separator);
-
-        // Verifica si la clave API está presente
-        if (apiKey != null && !apiKey.isEmpty()) {
-            try {
-                RestTemplate restTemplate = new RestTemplate();
-                String url = "https://www.googleapis.com/webfonts/v1/webfonts?key=" + apiKey;
-                Map<String, Object> response = restTemplate.getForObject(url, Map.class);
-
-                if (response != null && response.containsKey("items")) {
-                    List<Map<String, Object>> items = (List<Map<String, Object>>) response.get("items");
-
-                    for (Map<String, Object> item : items) {
-                        String family = (String) item.get("family");
-                        if (!combinedFonts.containsKey(family)) { // Añadir solo si no es parte de las combinaciones
-                            Map<String, String> fontEntry = new HashMap<>();
-                            fontEntry.put("font", family);
-                            fontEntry.put("pair", "");
-                            allFonts.add(fontEntry);
-                        }
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        return Map.of("fonts", allFonts);
+    // Añadir las combinaciones al principio
+    for (Map.Entry<String, String> entry : combinedFonts.entrySet()) {
+        Map<String, String> fontEntry = new HashMap<>();
+        fontEntry.put("font", entry.getKey());
+        fontEntry.put("pair", entry.getValue());
+        allFonts.add(fontEntry);
     }
+
+    // Añadir otras fuentes desde Google Fonts API aquí si es necesario
+
+    return Map.of("fonts", allFonts);
+}
 }
